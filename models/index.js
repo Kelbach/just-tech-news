ae39eb34ff8812741a9e694c86bd3e52cfd2da8b
kelbach <87092340+Kelbach@users.noreply.github.com>
2021-11-18 14:49:29 -0500

    13.4.6 creating a VOTE model and refactored routes to use Model functions

--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
 const User = require("./User");
 const Post = require("./Post");
+const Vote = require('./Vote');
 
-// create associations
+// create associations by identifying foreign key
 User.hasMany(Post, {
     foreignKey: 'user_id'
 });
@@ -10,4 +11,34 @@ Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
-module.exports = { User, Post };
+//these two below are for linking posts and users by votes
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+  
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
+//these 4 associations below link the Votes through each foreign key
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+  
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+  
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+  
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote };
